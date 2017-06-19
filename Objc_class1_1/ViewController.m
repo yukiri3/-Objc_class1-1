a1e1e4e0d98b0aa5868aeee140572091e6c6bcf8
Li Chen Liou <game412214@gmail.com>
2017-06-19 10:09:27 +0800

    add

--- Objc_class1_1/ViewController.m
+++ Objc_class1_1/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //假設一個變量int 作為計數用
+    int count;
+}
+//首先 在storyboard 擺一個Button 水平垂直置中
+//拉IBOutlet建立連結
+@property (strong, nonatomic) IBOutlet UIButton *myButton;
 
 @end
 
@@ -16,13 +23,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+     count=0;
 }
 
+- (IBAction)buttonAction:(id)sender {
+    //我們來寫一個簡單的練習
+    //當使用者按下去時 按鈕會變色 並且換字
+    if (count==0) {
+        //在上面IBOutlet中 我們命名為myButton
+        //設定背景顏色
+        [_myButton setBackgroundColor:[UIColor redColor]];
+        //設定按鈕title
+        [_myButton setTitle:@"Hello" forState:UIControlStateNormal];
+        count++;
+    }
+    else if(count==1){
+        [_myButton setBackgroundColor:[UIColor greenColor]];
+        [_myButton setTitle:@"World" forState:UIControlStateNormal];
+        count++;
+    }
+    else if(count==2){
+        [_myButton setBackgroundColor:[UIColor blueColor]];
+        [_myButton setTitle:@"Hello World" forState:UIControlStateNormal];
+        count=0;
+    }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
